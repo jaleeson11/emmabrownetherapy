ae441fe4b745fec97fd8723988a728cd363c0907
Joe Leeson <jaleeson11@gmail.com>
2023-08-12 13:45:39 +0100

    Checked for page/post content

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,43 +10,49 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="container">
-		<?php emmabrownetherapy_post_thumbnail(); ?>
+	<?php
+	if ( ! empty( get_the_content() ) ) :
+		?>
+		<div class="container">
+			<?php emmabrownetherapy_post_thumbnail(); ?>
 
-		<div class="entry-content">
-			<?php
-			the_content();
-
-			wp_link_pages(
-				array(
-					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'emmabrownetherapy' ),
-					'after'  => '</div>',
-				)
-			);
-			?>
-		</div><!-- .entry-content -->
-
-		<?php if ( get_edit_post_link() ) : ?>
-			<footer class="entry-footer">
+			<div class="entry-content">
 				<?php
-				edit_post_link(
-					sprintf(
-						wp_kses(
-							/* translators: %s: Name of current post. Only visible to screen readers */
-							__( 'Edit <span class="screen-reader-text">%s</span>', 'emmabrownetherapy' ),
-							array(
-								'span' => array(
-									'class' => array(),
-								),
-							)
-						),
-						wp_kses_post( get_the_title() )
-					),
-					'<span class="edit-link">',
-					'</span>'
+				the_content();
+
+				wp_link_pages(
+					array(
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'emmabrownetherapy' ),
+						'after'  => '</div>',
+					)
 				);
 				?>
-			</footer><!-- .entry-footer -->
-		<?php endif; ?>
-	</div><!-- .container -->
+			</div><!-- .entry-content -->
+
+			<?php if ( get_edit_post_link() ) : ?>
+				<footer class="entry-footer">
+					<?php
+					edit_post_link(
+						sprintf(
+							wp_kses(
+								/* translators: %s: Name of current post. Only visible to screen readers */
+								__( 'Edit <span class="screen-reader-text">%s</span>', 'emmabrownetherapy' ),
+								array(
+									'span' => array(
+										'class' => array(),
+									),
+								)
+							),
+							wp_kses_post( get_the_title() )
+						),
+						'<span class="edit-link">',
+						'</span>'
+					);
+					?>
+				</footer><!-- .entry-footer -->
+			<?php endif; ?>
+		</div><!-- .container -->
+		<?php
+	endif; 
+	?>
 </article><!-- #post-<?php the_ID(); ?> -->
