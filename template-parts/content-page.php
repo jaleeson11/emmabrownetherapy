1715438427f14bc76c7a66fd87f7eb08935c7cb3
Joe Leeson <jaleeson11@gmail.com>
2023-08-12 13:36:31 +0100

    Added container to page/post content

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,42 +10,43 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="container">
+		<?php emmabrownetherapy_post_thumbnail(); ?>
 
-	<?php emmabrownetherapy_post_thumbnail(); ?>
-
-	<div class="entry-content">
-		<?php
-		the_content();
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'emmabrownetherapy' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
+		<div class="entry-content">
 			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'emmabrownetherapy' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post( get_the_title() )
-				),
-				'<span class="edit-link">',
-				'</span>'
+			the_content();
+
+			wp_link_pages(
+				array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'emmabrownetherapy' ),
+					'after'  => '</div>',
+				)
 			);
 			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
+		</div><!-- .entry-content -->
+
+		<?php if ( get_edit_post_link() ) : ?>
+			<footer class="entry-footer">
+				<?php
+				edit_post_link(
+					sprintf(
+						wp_kses(
+							/* translators: %s: Name of current post. Only visible to screen readers */
+							__( 'Edit <span class="screen-reader-text">%s</span>', 'emmabrownetherapy' ),
+							array(
+								'span' => array(
+									'class' => array(),
+								),
+							)
+						),
+						wp_kses_post( get_the_title() )
+					),
+					'<span class="edit-link">',
+					'</span>'
+				);
+				?>
+			</footer><!-- .entry-footer -->
+		<?php endif; ?>
+	</div><!-- .container -->
 </article><!-- #post-<?php the_ID(); ?> -->
