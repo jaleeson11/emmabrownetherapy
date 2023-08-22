517898f3f0a503f924d7d41866a3ceeafd5f58dd
Joe Leeson <jaleeson11@gmail.com>
2023-08-22 14:43:00 +0100

    Modified posted on template tag

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -25,13 +25,13 @@ if ( ! function_exists( 'emmabrownetherapy_posted_on' ) ) :
 			esc_html( get_the_modified_date() )
 		);
 
-		$posted_on = sprintf(
-			/* translators: %s: post date. */
-			esc_html_x( 'Posted on %s', 'post date', 'emmabrownetherapy' ),
-			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-		);
+		// $posted_on = sprintf(
+		// 	/* translators: %s: post date. */
+		// 	esc_html_x( 'Posted on %s', 'post date', 'emmabrownetherapy' ),
+		// 	'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+		// );
 
-		echo '<span class="posted-on">' . $posted_on . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		echo '<span class="posted-on">' . $time_string . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 
 	}
 endif;
